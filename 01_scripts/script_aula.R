49954f0aa91b2ad217c6f0c213185533818d1935
Daniel Pagotto <danielppagotto@gmail.com>
2025-01-21 10:33:02 -0300

    update

---
+++ 01_scripts/script_aula.R
@@ -0,0 +1,68 @@
+
+# carregando os pacotes 
+library(tidyverse)
+library(readxl)
+library(readr)
+library(skimr)
+library(ggrepel)
+library(GGally)
+library(car)
+library(rstatix)
+
+# leitura dos dados
+
+GEM_APS <- read_excel("02_dados/GEM_APS.xlsx", 
+                      col_types = c("numeric", "text", "numeric", 
+                                    "numeric", "numeric", "numeric", "numeric", 
+                                    "numeric", "numeric", "numeric", "numeric", "numeric", 
+                                    "numeric", "numeric", "numeric", "numeric", "numeric", 
+                                    "numeric"))
+
+# usando a funcao glimpse (dplyr - tidyverse)
+
+glimpse(GEM_APS)
+
+# usando a funcao select 
+# o pipe |> ou %>% (atalho ctrl + shift + m)
+
+
+GEM_APS_selecionadas <- 
+  GEM_APS |> 
+  select(code, economy, year, 
+         tea, female_male_TEA)
+
+GEM_APS_selecionadas |> 
+  head()
+
+# usando a funcao filter 
+
+Gem_brazil <- GEM_APS |> 
+  filter(economy == "Brazil")
+
+Gem_brazil_argentina <- 
+  GEM_APS |> 
+  filter(economy == "Brazil" | 
+         economy == "Argentina")
+
+america_sul <- c("Brazil","Argentina","Chile",
+              "Colombia","Venezuela", "Peru")
+
+Gem_aps_america <- 
+  GEM_APS |> 
+  filter(economy %in% america_sul)
+
+## filtrar brasil e argentina e valores abaixo de < 2020
+
+gem_br_arg_menor_2020 <- 
+  GEM_APS |> 
+  filter((economy == "Brazil" | 
+         economy == "Argentina") & year < 2020)
+
+## Funcao mutate 
+
+GEM_APS |> 
+  select(economy, year, female_male_TEA) |> 
+  mutate(perc_fem_male_tea = female_male_TEA * 100) |> 
+  filter(year >= 2010 & economy == "Brazil") |> 
+  arrange(desc(year))
+
