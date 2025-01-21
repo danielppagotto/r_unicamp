c1ce18445e2aaa229c41541f416adf9c3ba30945
Daniel Pagotto <danielppagotto@gmail.com>
2025-01-21 11:06:01 -0300

    Update script_aula.R

--- 01_scripts/script_aula.R
+++ 01_scripts/script_aula.R
@@ -66,18 +66,35 @@ gem_br_arg_menor_2020 <-
 
 # Funcao mutate -----------------------------------------------------------
 
-
-GEM_APS |> 
+GEM_aps_perc <- 
+  GEM_APS |> 
   select(economy, year, female_male_TEA) |> 
   mutate(perc_fem_male_tea = female_male_TEA * 100) |> 
   filter(year >= 2010 & economy == "Brazil") |> 
   arrange(desc(year))
 
-
-
 # Funcoes group by e summarise --------------------------------------------
 
+unique(GEM_APS$economy)
 
+americas <- c("Brazil","Chile","Uruguay","Colombia","Canada",
+              "Ecuador","Mexico","United States","Venezuela")
+
+GEM23_americas <- 
+  GEM_APS |> 
+  filter(year == 2023) |> 
+  filter(economy %in% americas) |> 
+  mutate(hemisferio = 
+           if_else(economy == "Mexico"|
+                 economy == "Canada"|
+                 economy == "United States","Norte","Sul"),
+         .after = economy)
+
+GEM23_americas |> 
+  group_by(hemisferio) |> 
+  summarise(media = mean(tea),
+            mediana = median(tea),
+            desv_pad = sd(tea))
 
 
 # joins -------------------------------------------------------------------
